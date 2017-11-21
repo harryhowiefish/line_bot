53a4a20a141ae7272f05e4f0087335cf567de421
允涵 林 <harryhowiefish@jous-MacBook-Air-2.local>
2017-11-21 15:03:29 +0800

    bug fixed

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ var express = require('express');
 var mysql = require('mysql');
 const line = require('@line/bot-sdk');
 var schedule = require('node-schedule');
-var i=0;
-var start=0;
-var content;
-var type;
-var qresult_id;
 var channelAccessTokenvalue = "2VMlMDkyoFm35S+Zu936V5NUZTLReBHbwcvXfhIpP7rLtXnWgXY4bRAZbgrHtRZAgiWIS34dhJt7k9EXToy5CMrvmfPxQPwLy3P+kTvTy/hh2lh6dGwCspfip/eU7b9CBg39Q9nETYLwN1VNmGRwvQdB04t89/1O/w1cDnyilFU="
-var recorded;
-var updated;
+
 //-------------------------------------------------------------------------
 //連結linebot
 var bot = linebot({
@@ -43,27 +37,33 @@ con.connect(function(err) {
 
 bot.on('message', function(event) {
 	console.log("收到訊息");
+  var recorded;
+  var updated;
+  var input_error;
   var i=0;
   var start=0;
+  var content;
+  var type;
+  var qresult_id;
 	var userid = event.source.userId;
 	var message = event.message.text;
 
 	// simple check
 	// client.pushMessage(userid,{type: 'text',text: '收到'});
-  	con.query("SELECT * FROM `user` WHERE `userid` = '"+userid+"'", function (err, result) {
+  	con.query("SELECT `userid`,`flag`,`question`.`id`,`content` FROM `user` left join `question`on `user`.`question_num`=`question`.`id`WHERE `userid` = '"+userid+"'", function (err, result) {
   		if (err) throw err;
   		if(result.length!=0){
   			start = result[0].flag;
-  			i = result[0].question_num;
+  			i = result[0].id;
+        content = result[0].content;
   			console.log(userid+":"+i);
   			updated=true;
   			record();
   		}
 
-  	})
+  	});
 
-  	var checkid ="SELECT *  FROM `user` WHERE `userid` = '" + userid +"'"
-  	con.query(checkid, function (err, result) {
+  	con.query("SELECT *  FROM `user` WHERE `userid` = '" + userid +"'", function (err, result) {
   		if (err) throw err;
   		if(result.length === 0){
   			var sql = "INSERT INTO user (userid) VALUES ('"+userid +"')";
@@ -73,40 +73,39 @@ bot.on('message', function(event) {
        		});
        		client.pushMessage(userid,{type: 'text',text: '很高興認識你'});
    		};
-	});
+    });
 
 
   	if (userid==="U05a02f4a949c84fd19afebe7483a2e84" && message==="開始問卷"){
   		console.log('start');
-  		var alluser ="SELECT *  FROM `user`";
+  		var alluser ="SELECT * FROM `user`";
   		con.query(alluser, function (err, result) {
   			if (err) throw err;
   			con.query("SELECT `content`,`type`  FROM `question` WHERE id = 1", function (err, result2) {
-
 		  		var j=result.length;
 	  			for (k=0; k<j; k++){
-	  				userid = result[k].userid;
-			   		con.query("UPDATE `user` SET `flag`=1, `question_num`=1 WHERE userid = '"+userid+"'", function (err, result) {if (err) throw err;})
+            userid=result[k].userid;
+			   		con.query("UPDATE `user` SET `flag`=1, `question_num`=1 WHERE userid = '"+userid+"'", function (err, result) {
+              if (err) throw err;
+            });
+            client.pushMessage(userid,{type: 'text',text: "你可以開始問卷了"});
+            start=1;
 		    		con.query("INSERT INTO question_result (userid) VALUES ('"+userid +"')", function (err, result) {if (err) throw err;})
-		    		client.pushMessage(result[k].userid,{type: 'text',text: "你可以開始問卷了"});
-			   		start=1;
-			   		i=1;
-			   		content = result2[0].content;
-	  				type = result2[0].type;
-		  			client.pushMessage(userid,{type: 'text',text: content});
-				};
-
+            content = result2[0].content;
+            type = result2[0].type;
+            start_questionaire();
+				  };
 	  		});
   		});
   	};
 
-function record(){
+  function record(){
 		if(updated){
 			if(start === 1){
-	  			console.log("接收答案");
-			  	if(message >=1 && message <=10 && type==="scale"){
-			  		con.query("INSERT INTO msg_log (userid, question_id, msg) VALUES ('"+userid +"',"+ i+",'"+message+"')", function (err, result) {if (err) throw err;});
-					con.query("SELECT max(id) as id  from `question_result` where userid='"+userid+"'",function(err,result){
+			  	if(message >=1 && message <=10 ){
+            console.log("接收答案");
+			  	  con.query("INSERT INTO msg_log (userid, question_id, msg) VALUES ('"+userid +"',"+ i+",'"+message+"')", function (err, result) {if (err) throw err;});
+            con.query("SELECT max(id) as id  from `question_result` where userid='"+userid+"'",function(err,result){
 						if (err) throw err;
 						qresult_id=result[0].id;
 						con.query("UPDATE `question_result` SET `Q_"+i+"`="+message+" WHERE `id` ='"+qresult_id+"'", function (err, result) {
@@ -118,7 +117,8 @@ function record(){
 			  	}
 			  	else{
 			  		client.pushMessage(userid,{type: 'text',text: "資料錯誤，請重新填寫"});
-
+            input_error=true;
+            askagain();
 	  			};
 	  		}
 	  		else{
@@ -126,42 +126,56 @@ function record(){
 	  		};
 	  	};
 	};
+
 	function ask(){
 		if(updated){
-  			con.query("SELECT * FROM `user` WHERE `userid` = '"+userid+"'", function (err, result) {
-	  				if (err) throw err;
-	  					i = result[0].question_num;
-		  				if (i <=10 && i>=1){
-		  					con.query("SELECT `content`,`type`  FROM `question` WHERE id = "+i, function (err, result) {
-		  						if (err) throw err;
-		  						content = result[0].content;
-		  						type = result[0].type;
-				  				client.pushMessage(userid,{type: 'text',text: content});
-				  			})
-				  		}
-				  		if(i ===11){
-				  			client.pushMessage(userid,{type: 'text',text: "問卷完成囉"});
-					   		con.query("UPDATE `user` SET `flag`=0, `question_num`=0 WHERE userid = '"+userid+"'", function (err, result) {if (err) throw err;})
-						}
+		  if (i <=10 && i>=1){
+        con.query("SELECT `content`,`type`  FROM `question` WHERE id = "+i , function (err, result) {
+          content=result[0].content;
+		  	  client.pushMessage(userid,{type: 'text',text: content});
+		  	});
+      }
+			if(i ===11){
+				client.pushMessage(userid,{type: 'text',text: "問卷完成囉"});
+					con.query("UPDATE `user` SET `flag`=0, `question_num`=0 WHERE userid = '"+userid+"'", function (err, result) {if (err) throw err;})
+			}
 	
-			});
+		};
 			updated=false;
+  };
 
-  		}
-  	};
+  function askagain(){
+    if(input_error){
+      if (i <=10 && i>=1){
+        client.pushMessage(userid,{type: 'text',text: content});
+      }
+      if(i ===11){
+        client.pushMessage(userid,{type: 'text',text: "問卷完成囉"});
+          con.query("UPDATE `user` SET `flag`=0, `question_num`=0 WHERE userid = '"+userid+"'", function (err, result) {if (err) throw err;})
+      }
+  
+    };
+      input_error=false;
+  };
 
-
-  	function next_q(){
-  		if(recorded=true){
-  			i++;
-  			recorded=false;
-  			con.query("UPDATE `user` SET `flag`=1, `question_num`="+i+" WHERE `userid` = '"+userid+"'", function (err, result) {if (err) throw err;});
-
-  		}
-  		ask();
-
+  function next_q(){
+		if(recorded=true){
+ 		  i++;
+      console.log(i);
+		  recorded=false;
+		  con.query("UPDATE `user` SET `flag`=1, `question_num`="+i+" WHERE `userid` = '"+userid+"'", function (err, result) {if (err) throw err;});
   	}
-  });
+  	ask();
+    console.log("this comes after i");
+  };
+
+  function start_questionaire(){
+    if(start===1){
+      client.pushMessage(userid,{type: 'text',text: content});
+    }
+  }
+
+});
 	
 	
 
